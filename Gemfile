465c295324ccf3c24ae8a04b1f59786f377fe65c
BananaOnTheWall <niushineshine@gmail.com>
2014-03-14 22:43:27 +0800

    postgre added

--- Gemfile
+++ Gemfile
@@ -2,10 +2,10 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.3'
+gem 'pg'
 
 group :development, :test do
-# Use sqlite3 as the database for Active Record
-    gem 'sqlite3'
+# Use postgresql as the database for Active Record    
     gem 'rspec-rails'
 end
 
@@ -38,10 +38,6 @@ gem 'jbuilder', '~> 1.2'
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
-end
-
-group :production do
-    gem 'pg'
 end
 
 # Use ActiveModel has_secure_password
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:title_prefix) {'Ruby on Rails Tutorial Sample App'}
+  
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -9,7 +12,7 @@ describe "StaticPages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{title_prefix} | Home")
     end
   end
 
@@ -21,7 +24,7 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{title_prefix} | Help")
     end
   end
 
@@ -33,7 +36,14 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+      expect(page).to have_title("#{title_prefix} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{title_prefix} | Contact Us")
     end
   end
 end
